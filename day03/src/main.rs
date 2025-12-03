469c05bc11afd309feac1e95c07cf7e32f23b672
Juan Manuel Gimeno <jmgimeno@gmail.com>
2025-12-03 11:15:47 +0100

    refactor maximum_joltage function to support variable window length

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -22,90 +22,15 @@ impl FromStr for Bank {
 }
 
 impl Bank {
-    fn maximum_joltage_old(&self) -> u32 {
-        let mut bf = self.batteries[0].joltage;
-        let mut max = 0;
-        for s in 1..self.batteries.len() {
-            let bs = self.batteries[s].joltage;
-            max = max.max(bf * 10 + bs);
-            if bf < bs {
-                bf = bs;
-            }
-        }
-        max
-    }
-
-    fn maximum_joltage_step1(&self) -> u32 {
-        let mut idxs = [0, 1];
-        for i in 2..self.batteries.len() {
-            let bf = self.batteries[idxs[0]].joltage;
-            let bs = self.batteries[idxs[1]].joltage;
-            let bi = self.batteries[i].joltage;
-            if bs > bf {
-                idxs[0] = idxs[1];
-                idxs[1] = i;
-            } else if bi > bs {
-                idxs[1] = i;
-            }
-        }
-        self.batteries[idxs[0]].joltage * 10 + self.batteries[idxs[1]].joltage
-    }
-
-    fn maximum_joltage_step2(&self) -> u32 {
-        let mut max = [0, 1].map(|i| self.batteries[i].joltage);
-        for b in &self.batteries[2..] {
-            let bj = b.joltage;
-            if max[1] > max[0] {
-                max[0] = max[1];
-                max[1] = bj;
-            } else if bj > max[1] {
-                max[1] = bj;
-            }
-        }
-        max[0] * 10 + max[1]
-    }
-
-    fn maximum_joltage_step3(&self) -> u32 {
-        let mut max = [0, 1]
-            .iter()
-            .map(|i| self.batteries[*i].joltage)
-            .collect::<Vec<_>>();
-        for b in &self.batteries[2..] {
-            max.push(b.joltage);
-            let mut removed = false;
-            for i in 1..max.len() {
-                // I need to find if there is a position in the prefix 0..i that
-                // has a value that is lower than max[i]
-                match (0..i).rev().find(|j| max[*j] < max[i]) {
-                    Some(j) => {
-                        removed = true;
-                        max.remove(j);
-                        break;
-                    }
-                    None => (),
-                }
-            }
-            if !removed {
-                max.pop();
-            }
-        }
-        max[0] * 10 + max[1]
-    }
-
-    fn maximum_joltage(&self) -> u32 {
-        self.maximum_joltage_step4(2)
-    }
-
-    fn maximum_joltage_step4(&self, len: usize) -> u32 {
-        let mut max =
-            (0..len)
+    fn maximum_joltage(&self, window_len: usize) -> u64 {
+        let mut max = (0..window_len)
             .map(|i| self.batteries[i].joltage)
             .collect::<Vec<_>>();
-        for b in &self.batteries[len..] {
+        for b in &self.batteries[window_len..] {
             max.push(b.joltage);
             let mut removed = false;
             for i in 1..max.len() {
-                // I need to find if there is a position in the prefix 0..i that
+                // I need to find if there is a position in the prefix before i that
                 // has a value that is lower than max[i]
                 match (0..i).rev().find(|j| max[*j] < max[i]) {
                     Some(j) => {
@@ -121,7 +46,7 @@ impl Bank {
             }
         }
 
-        max.iter().fold(0, |acc, &x| 10 * acc + x)
+        max.iter().fold(0, |acc, &x| 10 * acc + x as u64)
     }
 }
 
@@ -144,12 +69,12 @@ fn main() {
     println!("Part2: {}", part2(&INPUT));
 }
 
-fn part1(input: &[Bank]) -> u32 {
-    input.iter().map(|b| b.maximum_joltage()).sum()
+fn part1(input: &[Bank]) -> u64 {
+    input.iter().map(|b| b.maximum_joltage(2)).sum()
 }
 
-fn part2(input: &[Bank]) -> usize {
-    input.len()
+fn part2(input: &[Bank]) -> u64 {
+    input.iter().map(|b| b.maximum_joltage(12)).sum()
 }
 
 #[cfg(test)]
@@ -158,10 +83,22 @@ mod tests {
 
     #[test]
     fn test_examples_part1() {
-        assert_eq!("987654321111111".parse::<Bank>().unwrap().maximum_joltage(), 98);
-        assert_eq!("811111111111119".parse::<Bank>().unwrap().maximum_joltage(), 89);
-        assert_eq!("234234234234278".parse::<Bank>().unwrap().maximum_joltage(), 78);
-        assert_eq!("818181911112111".parse::<Bank>().unwrap().maximum_joltage(), 92);
+        assert_eq!(
+            "987654321111111".parse::<Bank>().unwrap().maximum_joltage(2),
+            98
+        );
+        assert_eq!(
+            "811111111111119".parse::<Bank>().unwrap().maximum_joltage(2),
+            89
+        );
+        assert_eq!(
+            "234234234234278".parse::<Bank>().unwrap().maximum_joltage(2),
+            78
+        );
+        assert_eq!(
+            "818181911112111".parse::<Bank>().unwrap().maximum_joltage(2),
+            92
+        );
     }
 
     #[test]
@@ -170,7 +107,27 @@ mod tests {
     }
 
     #[test]
+    fn test_examples_part2() {
+        assert_eq!(
+            "987654321111111".parse::<Bank>().unwrap().maximum_joltage(12),
+            987654321111
+        );
+        assert_eq!(
+            "811111111111119".parse::<Bank>().unwrap().maximum_joltage(12),
+            811111111119
+        );
+        assert_eq!(
+            "234234234234278".parse::<Bank>().unwrap().maximum_joltage(12),
+            434234234278
+        );
+        assert_eq!(
+            "818181911112111".parse::<Bank>().unwrap().maximum_joltage(12),
+            888911112111
+        );
+    }
+
+    #[test]
     fn test_part2() {
-        assert_eq!(part2(&INPUT), 20);
+        assert_eq!(part2(&INPUT), 172516781546707);
     }
 }
