ca0b58c9b097ab6d88507a35d7bd556b51603d99
Juan Manuel Gimeno <jmgimeno@gmail.com>
2025-12-03 10:55:26 +0100

    refactoring maximum_joltage step by step

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -22,7 +22,7 @@ impl FromStr for Bank {
 }
 
 impl Bank {
-    fn maximum_joltage(&self) -> u32 {
+    fn maximum_joltage_old(&self) -> u32 {
         let mut bf = self.batteries[0].joltage;
         let mut max = 0;
         for s in 1..self.batteries.len() {
@@ -33,6 +33,95 @@ impl Bank {
             }
         }
         max
+    }
+
+    fn maximum_joltage_step1(&self) -> u32 {
+        let mut idxs = [0, 1];
+        for i in 2..self.batteries.len() {
+            let bf = self.batteries[idxs[0]].joltage;
+            let bs = self.batteries[idxs[1]].joltage;
+            let bi = self.batteries[i].joltage;
+            if bs > bf {
+                idxs[0] = idxs[1];
+                idxs[1] = i;
+            } else if bi > bs {
+                idxs[1] = i;
+            }
+        }
+        self.batteries[idxs[0]].joltage * 10 + self.batteries[idxs[1]].joltage
+    }
+
+    fn maximum_joltage_step2(&self) -> u32 {
+        let mut max = [0, 1].map(|i| self.batteries[i].joltage);
+        for b in &self.batteries[2..] {
+            let bj = b.joltage;
+            if max[1] > max[0] {
+                max[0] = max[1];
+                max[1] = bj;
+            } else if bj > max[1] {
+                max[1] = bj;
+            }
+        }
+        max[0] * 10 + max[1]
+    }
+
+    fn maximum_joltage_step3(&self) -> u32 {
+        let mut max = [0, 1]
+            .iter()
+            .map(|i| self.batteries[*i].joltage)
+            .collect::<Vec<_>>();
+        for b in &self.batteries[2..] {
+            max.push(b.joltage);
+            let mut removed = false;
+            for i in 1..max.len() {
+                // I need to find if there is a position in the prefix 0..i that
+                // has a value that is lower than max[i]
+                match (0..i).rev().find(|j| max[*j] < max[i]) {
+                    Some(j) => {
+                        removed = true;
+                        max.remove(j);
+                        break;
+                    }
+                    None => (),
+                }
+            }
+            if !removed {
+                max.pop();
+            }
+        }
+        max[0] * 10 + max[1]
+    }
+
+    fn maximum_joltage(&self) -> u32 {
+        self.maximum_joltage_step4(2)
+    }
+
+    fn maximum_joltage_step4(&self, len: usize) -> u32 {
+        let mut max =
+            (0..len)
+            .map(|i| self.batteries[i].joltage)
+            .collect::<Vec<_>>();
+        for b in &self.batteries[len..] {
+            max.push(b.joltage);
+            let mut removed = false;
+            for i in 1..max.len() {
+                // I need to find if there is a position in the prefix 0..i that
+                // has a value that is lower than max[i]
+                match (0..i).rev().find(|j| max[*j] < max[i]) {
+                    Some(j) => {
+                        removed = true;
+                        max.remove(j);
+                        break;
+                    }
+                    None => (),
+                }
+            }
+            if !removed {
+                max.pop();
+            }
+        }
+
+        max.iter().fold(0, |acc, &x| 10 * acc + x)
     }
 }
 
