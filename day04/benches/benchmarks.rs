a05227c063ec141950ceedf3e15398a17820a681
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-12-05 16:47:12 +0100

    add benchmarks for day04 and update project structure

---
+++ day04/benches/benchmarks.rs
@@ -0,0 +1,15 @@
+use day04::day04::{INPUT, part1, part2};
+
+fn main() {
+    divan::main()
+}
+
+#[divan::bench]
+fn bench_part1() {
+    part1(divan::black_box(&INPUT));
+}
+
+#[divan::bench]
+fn bench_part2() {
+    part2(divan::black_box(&INPUT));
+}
