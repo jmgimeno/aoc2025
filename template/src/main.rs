cd23b94ddd953b74829ad57ad08713b20d457b22
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-12-05 17:34:16 +0100

    update template structure to include benchmarks

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,33 +1,7 @@
-use common::read_file_as_string;
-use once_cell::sync::Lazy;
-
-static INPUT: Lazy<String> =
-    Lazy::new(|| read_file_as_string("data/{{project-name}}.txt").expect("Failed to load input"));
+mod {{project-name}};
+use crate::{{project-name}}::{INPUT, part1, part2};
 
 fn main() {
     println!("Part1: {}", part1(&INPUT));
     println!("Part2: {}", part2(&INPUT));
-}
-
-fn part1(_input: &str) -> usize {
-    todo!("{{project-name}} - part1")
-}
-
-fn part2(_input: &str) -> usize {
-    todo!("{{project-name}} - part1")
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1(&INPUT), 0);
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(&INPUT), 0);
-    }
 }
---
+++ template/src/{{project-name}}.rs
@@ -0,0 +1,28 @@
+use common::read_file_as_string;
+use once_cell::sync::Lazy;
+
+pub static INPUT: Lazy<String> =
+    Lazy::new(|| read_file_as_string("data/{{project-name}}.txt").expect("Failed to load input"));
+
+pub fn part1(_input: &str) -> usize {
+    todo!("{{project-name}} - part1")
+}
+
+pub fn part2(_input: &str) -> usize {
+    todo!("{{project-name}} - part1")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&INPUT), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&INPUT), 0);
+    }
+}
