bffdc4e0ec14c0065991c1d7ad974a8048b5c016
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-12-05 14:01:55 +0100

    add benchmarks to day05

---
+++ day05/benches/benchmarks.rs
@@ -0,0 +1,16 @@
+use std::ops::Deref;
+use day05::day05::{INPUT, part1, part2};
+
+fn main() {
+    divan::main()
+}
+
+#[divan::bench]
+fn bench_part1() {
+    part1(divan::black_box(&INPUT));
+}
+
+#[divan::bench]
+fn bench_part2() {
+    part2(divan::black_box(&INPUT));
+}
