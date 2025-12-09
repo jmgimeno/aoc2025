b9a67a704d62957dc94b0c686d69a70e5dd82f10
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-12-09 19:15:04 +0100

    feat(day09): implement part2 functionality and add tests for IsInside struct

--- day09/src/day09.rs
+++ day09/src/day09.rs
@@ -1,5 +1,9 @@
 use common::read_file_as_elements;
+use itertools::Itertools;
 use once_cell::sync::Lazy;
+use std::collections::BTreeMap;
+use std::ops::Bound::{Excluded, Unbounded};
+use std::ops::RangeInclusive;
 use std::str::FromStr;
 
 pub static INPUT: Lazy<Vec<Point>> =
@@ -83,8 +87,93 @@ pub fn part1(tiles: &[Point]) -> u64 {
     max_area
 }
 
-pub fn part2(_tiles: &[Point]) -> usize {
-    todo!("day09 - part1")
+#[derive(Debug)]
+struct IsInside {
+    // for each x it gives the vertical edges at that x
+    vertical_edges: BTreeMap<i32, Vec<RangeInclusive<i32>>>,
+    // for each y it gives the horizontal edhes at that y
+    horizontal_edges: BTreeMap<i32, Vec<RangeInclusive<i32>>>,
+}
+
+impl IsInside {
+    fn new(points: &[Point]) -> Self {
+        Self {
+            vertical_edges: points
+                .iter()
+                .circular_tuple_windows()
+                .filter_map(|(p1, p2)| {
+                    (p1.x == p2.x)
+                        .then_some((p1.x, std::cmp::min(p1.y, p2.y)..=std::cmp::max(p1.y, p2.y)))
+                })
+                .into_group_map_by(|(x, _)| *x)
+                .into_iter()
+                .map(|(x, pairs)| (x, pairs.into_iter().map(|(_, r)| r).collect()))
+                .collect::<BTreeMap<_, _>>(),
+            horizontal_edges: points
+                .iter()
+                .circular_tuple_windows()
+                .filter_map(|(p1, p2)| {
+                    (p1.y == p2.y)
+                        .then_some((p1.y, std::cmp::min(p1.x, p2.x)..=std::cmp::max(p1.x, p2.x)))
+                })
+                .into_group_map_by(|(y, _)| *y)
+                .into_iter()
+                .map(|(x, pairs)| (x, pairs.into_iter().map(|(_, r)| r).collect()))
+                .collect::<BTreeMap<_, _>>(),
+        }
+    }
+
+    fn is_inside(&self, point: &Point) -> bool {
+        let Point { x, y } = point;
+        // If it's on any vertical edge is inside (this accounts for vertices as well)
+        if self
+            .vertical_edges
+            .get(x)
+            .map(|ranges| ranges.iter().any(|range| range.contains(y)))
+            .unwrap_or(false)
+        {
+            return true;
+        }
+        // If it's on any horizontal edge is inside
+        if self
+            .horizontal_edges
+            .get(y)
+            .map(|ranges| ranges.iter().any(|range| range.contains(x)))
+            .unwrap_or(false)
+        {
+            return true;
+        }
+        // If not, we use ray-casting (to the infinite x)
+        let mut crosses = 0;
+        for (_xx, ranges) in self.vertical_edges.range((Excluded(x), Unbounded)) {
+            if ranges.iter().any(|r| r.contains(y)) { // we cross a vertical edge at (_xx, y)
+                crosses += 1;
+            }
+        }
+        crosses % 2 == 1
+    }
+}
+
+pub fn part2(tiles: &[Point]) -> u64 {
+    let is_inside = IsInside::new(&tiles);
+    let mut max_area = 0;
+    for (i, tile1) in tiles.iter().enumerate() {
+        for tile2 in &tiles[i + 1..] {
+            let other1 = Point {
+                x: tile1.x,
+                y: tile2.y,
+            };
+            let other2 = Point {
+                x: tile2.x,
+                y: tile1.y,
+            };
+            // Is this condition enough to determine if all the square is inside?
+            if is_inside.is_inside(&other1) && is_inside.is_inside(&other2) {
+                max_area = std::cmp::max(max_area, tile1.area(tile2));
+            }
+        }
+    }
+    max_area
 }
 
 #[cfg(test)]
@@ -108,13 +197,52 @@ mod tests {
             .collect::<Vec<Point>>();
         assert_eq!(part1(&tiles), 50);
     }
+
     #[test]
     fn test_part1() {
         assert_eq!(part1(&INPUT), 4750176210);
     }
 
     #[test]
+    fn test_example_part2() {
+        let input = "\
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3";
+        let tiles = input
+            .lines()
+            .map(|l| l.parse().unwrap())
+            .collect::<Vec<Point>>();
+        assert_eq!(part2(&tiles), 24);
+    }
+
+    #[test]
     fn test_part2() {
         todo!("day09 - test - part2")
+    }
+
+    #[test]
+    fn test_example_is_inside() {
+        let input = "\
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3";
+        let tiles = input
+            .lines()
+            .map(|l| l.parse().unwrap())
+            .collect::<Vec<Point>>();
+        let vertical_lines = IsInside::new(&tiles);
+        assert!(!vertical_lines.is_inside(&Point { x: 2, y: 1 }));
+        assert!(vertical_lines.is_inside(&Point { x: 9, y: 3 }));
     }
 }
