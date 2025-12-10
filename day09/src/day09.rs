02a3a541646443e276560772ef47de1567f6a630
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-12-10 12:17:24 +0100

    implement part2 following Chris Biscardi's idea

--- day09/src/day09.rs
+++ day09/src/day09.rs
@@ -1,9 +1,6 @@
 use common::read_file_as_elements;
 use itertools::Itertools;
 use once_cell::sync::Lazy;
-use std::collections::BTreeMap;
-use std::ops::Bound::{Excluded, Unbounded};
-use std::ops::RangeInclusive;
 use std::str::FromStr;
 
 pub static INPUT: Lazy<Vec<Point>> =
@@ -81,95 +78,53 @@ pub fn part1(tiles: &[Point]) -> u64 {
     let mut max_area = 0;
     for (i, tile1) in external.iter().enumerate() {
         for tile2 in &external[i + 1..] {
-            max_area = std::cmp::max(max_area, tile1.area(tile2));
+            max_area = max_area.max(tile1.area(tile2));
         }
     }
     max_area
 }
 
 #[derive(Debug)]
-struct IsInside {
-    // for each x it gives the vertical edges at that x
-    vertical_edges: BTreeMap<i32, Vec<RangeInclusive<i32>>>,
-    // for each y it gives the horizontal edhes at that y
-    horizontal_edges: BTreeMap<i32, Vec<RangeInclusive<i32>>>,
+struct InsideDetector<'a> {
+    lines: Vec<(&'a Point, &'a Point)>,
 }
 
-impl IsInside {
-    fn new(points: &[Point]) -> Self {
+impl<'a> InsideDetector<'a> {
+    fn new(points: &'a [Point]) -> Self {
         Self {
-            vertical_edges: points
+            lines: points
                 .iter()
                 .circular_tuple_windows()
-                .filter_map(|(p1, p2)| {
-                    (p1.x == p2.x)
-                        .then_some((p1.x, std::cmp::min(p1.y, p2.y)..=std::cmp::max(p1.y, p2.y)))
-                })
-                .into_group_map_by(|(x, _)| *x)
-                .into_iter()
-                .map(|(x, pairs)| (x, pairs.into_iter().map(|(_, r)| r).collect()))
-                .collect::<BTreeMap<_, _>>(),
-            horizontal_edges: points
-                .iter()
-                .circular_tuple_windows()
-                .filter_map(|(p1, p2)| {
-                    (p1.y == p2.y)
-                        .then_some((p1.y, std::cmp::min(p1.x, p2.x)..=std::cmp::max(p1.x, p2.x)))
-                })
-                .into_group_map_by(|(y, _)| *y)
-                .into_iter()
-                .map(|(x, pairs)| (x, pairs.into_iter().map(|(_, r)| r).collect()))
-                .collect::<BTreeMap<_, _>>(),
+                .collect::<Vec<(_, _)>>(),
         }
     }
 
-    fn is_inside(&self, point: &Point) -> bool {
-        let Point { x, y } = point;
-        // If it's on any vertical edge is inside (this accounts for vertices as well)
-        if self
-            .vertical_edges
-            .get(x)
-            .map(|ranges| ranges.iter().any(|range| range.contains(y)))
-            .unwrap_or(false)
-        {
-            return true;
-        }
-        // If it's on any horizontal edge is inside
-        if self
-            .horizontal_edges
-            .get(y)
-            .map(|ranges| ranges.iter().any(|range| range.contains(x)))
-            .unwrap_or(false)
-        {
-            return true;
-        }
-        // If not, we use ray-casting (to the infinite x)
-        let mut crosses = 0;
-        for (_xx, ranges) in self.vertical_edges.range((Excluded(x), Unbounded)) {
-            if ranges.iter().any(|r| r.contains(y)) { // we cross a vertical edge at (_xx, y)
-                crosses += 1;
-            }
-        }
-        crosses % 2 == 1
+    // Chris Biscardi's idea
+    fn is_valid(&self, p1: &Point, p2: &Point) -> bool {
+        let min_x = p1.x.min(p2.x);
+        let max_x = p1.x.max(p2.x);
+        let min_y = p1.y.min(p2.y);
+        let max_y = p1.y.max(p2.y);
+        self.lines.iter().all(|(l1, l2)| {
+            let min_l_x = l1.x.min(l2.x);
+            let max_l_x = l1.x.max(l2.x);
+            let min_l_y = l1.y.min(l2.y);
+            let max_l_y = l1.y.max(l2.y);
+            max_x <= min_l_x // line on the right
+                || min_x >= max_l_x // line on the left
+                || max_y <= min_l_y // line below
+                || min_y >= max_l_y // line above
+        })
     }
 }
 
 pub fn part2(tiles: &[Point]) -> u64 {
-    let is_inside = IsInside::new(&tiles);
+    let detector = InsideDetector::new(tiles);
     let mut max_area = 0;
-    for (i, tile1) in tiles.iter().enumerate() {
-        for tile2 in &tiles[i + 1..] {
-            let other1 = Point {
-                x: tile1.x,
-                y: tile2.y,
-            };
-            let other2 = Point {
-                x: tile2.x,
-                y: tile1.y,
-            };
-            // Is this condition enough to determine if all the square is inside?
-            if is_inside.is_inside(&other1) && is_inside.is_inside(&other2) {
-                max_area = std::cmp::max(max_area, tile1.area(tile2));
+    for (i, p1) in tiles.iter().enumerate() {
+        for p2 in &tiles[i + 1..] {
+            if detector.is_valid(p1, p2) {
+                max_area = max_area.max(p1.area(&p2));
             }
         }
     }
@@ -223,26 +178,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        todo!("day09 - test - part2")
-    }
-
-    #[test]
-    fn test_example_is_inside() {
-        let input = "\
-7,1
-11,1
-11,7
-9,7
-9,5
-2,5
-2,3
-7,3";
-        let tiles = input
-            .lines()
-            .map(|l| l.parse().unwrap())
-            .collect::<Vec<Point>>();
-        let vertical_lines = IsInside::new(&tiles);
-        assert!(!vertical_lines.is_inside(&Point { x: 2, y: 1 }));
-        assert!(vertical_lines.is_inside(&Point { x: 9, y: 3 }));
+        assert_eq!(part2(&INPUT), 1574684850);
     }
 }
